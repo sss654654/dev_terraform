5e4ddb0b31cf6f84d4065e946dadeda5e8622f86
subin <subinhong0109@dankook.ac.kr>
2025-08-15 15:33:05 +0900

    eks용 vpc-subnet에 tag 삭제

--- infra/VPC-Subnet.tf
+++ infra/VPC-Subnet.tf
@@ -1,5 +1,3 @@
-# dev_terraform-main\infra\VPC-Subnet.tf
-
 # VPC 생성
 resource "aws_vpc" "dev_vpc" {
   cidr_block = "10.0.0.0/16"
@@ -7,7 +5,6 @@ resource "aws_vpc" "dev_vpc" {
   enable_dns_support   = true
   tags = {
     Name = "dev-vpc"
-    "kubernetes.io/cluster/dev-eks-cluster" = "shared"
   }
 }
 
@@ -16,7 +13,6 @@ resource "aws_internet_gateway" "dev_igw" {
   vpc_id = aws_vpc.dev_vpc.id
   tags = {
     Name = "dev-igw"
-    "kubernetes.io/cluster/dev-eks-cluster" = "shared"
   }
 }
 
@@ -28,8 +24,6 @@ resource "aws_subnet" "public_subnet" {
   map_public_ip_on_launch = true # EC2 생성 시 퍼블릭 IP 자동 할당
   tags = {
     Name = "dev-public-subnet"
-    "kubernetes.io/role/elb" = "1"
-    "kubernetes.io/cluster/dev-eks-cluster" = "owned"
   }
 }
 
@@ -54,7 +48,6 @@ resource "aws_subnet" "db_private_subnet" {
   availability_zone = "ap-northeast-2a"
   tags = {
     Name = "dev-db-private-subnet"
-    "kubernetes.io/cluster/dev-eks-cluster" = "shared"
   }
 }
 
@@ -65,8 +58,6 @@ resource "aws_subnet" "eks_fargate_private_subnet" {
   availability_zone = "ap-northeast-2a"
   tags = {
     Name = "dev-eks-fargate-private-subnet"
-    "kubernetes.io/role/internal-elb" = "1"
-    "kubernetes.io/cluster/dev-eks-cluster" = "owned"
   }
 }
 
@@ -80,3 +71,4 @@ resource "aws_subnet" "gitlab_private_subnet" {
     Name = "dev-gitlab-private-subnet"
   }
 }
+
